5b26a103c00e16c161f9490c1fb473d8d2dc2f8a
NICKOLLAS MATHEUS AMORIM DE SOUZA <07076852144@ifms.edu.br>
2024-07-25 22:23:44 -0400

    subindo codigo

--- src/main/java/com/anpede/TadsAnpedeApplication.java
+++ src/main/java/com/anpede/TadsAnpedeApplication.java
@@ -2,17 +2,19 @@ package com.anpede;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EnableWebMvc	 
-public class TadsAnpedeApplication implements WebMvcConfig {
+@EnableWebMvc
+public class TadsAnpedeApplication implements WebMvcConfigurer {
 	
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-			registry.addMapping("/**")
-				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
-			}
-
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TadsAnpedeApplication.class, args);
 	}
